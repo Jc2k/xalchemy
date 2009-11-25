713e8110087e45e005837a621e842cc93d627ece
John Carr <john.carr@unrouted.co.uk>
2009-11-25 08:44:49 +0000

    Add some tests

---
+++ tests/test_ast.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from xquery.ast import *
+
+class TestAst(TestCase):
+
+    def test_variable(self):
+        self.assertEquals(Variable("foo").get_string(), "$foo")
+
+    def test_context(self):
+        self.assertEquals(Context(), ".")
+
+    def test_element(self):
+        self.assertEquals(Element("some:ele").get_string(), "some:ele")

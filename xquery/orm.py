60ff02d29c38e886d30daa8fa721df31bab29fd5
John Carr <john.carr@unrouted.co.uk>
2009-11-27 19:55:29 +0000

    Add a basic outline for how Document.find() will work

---
+++ xquery/orm.py
@@ -0,0 +1,30 @@
+
+from .ast import *
+
+
+class Query(object):
+    """ Represents an unexecuted query """
+
+    def __init__(self, cls, expression):
+        self.cls = cls
+        self.expression = expression
+
+    def get_xquery(self):
+        return self.expression.get_string()
+
+    def __iter__(self):
+        raise StopIteration
+
+
+class Document(object):
+    """ Represents an XML document stored in an XQuery store """
+
+    def __init__(self, *args):
+        pass
+
+    @classmethod
+    def find(self, *args):
+        v = Variable("retval")
+        f = FLOWR(v, Context(), v)
+        return Query(cls, f)
+
